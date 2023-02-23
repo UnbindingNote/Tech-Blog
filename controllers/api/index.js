f28f9b8c10f01794f7d9cd1d8dd41183086968a7
UnbindingNote <[200~111218283+UnbindingNote@users.noreply.github.com~>
2023-02-23 06:56:19 -0500

    cannibalizing project 2 code for starter code, needs some fleshing out and moving about to look diff.

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+// const bakedRoutes = require('./bakedRoutes');
+
+router.use('/users', userRoutes);
+// router.use('/bakedGoods', bakedRoutes);
+
+module.exports = router;
