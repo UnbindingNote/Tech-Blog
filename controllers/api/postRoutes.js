5d6cdc177326b85a724ea9cf1dc6c4f819f03449
UnbindingNote <[200~111218283+UnbindingNote@users.noreply.github.com~>
2023-02-23 20:03:19 -0500

    removed broken handlebars, leaving routes as-is and working on another homework. might come back and finish this before class ends.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,24 @@
 // need something for root and then finding by id? 
 // do I bring in comments? figure it out
 // GET/POST/DELETE probably
+
+const router = require('express').Router();
+const { Post } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+router.get();
+router.post('/', withAuth, async (req, res)=>{
+    try {
+        const newPost = await Post.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
+        res.status(200).json(newPost);
+        
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+});
+router.delete();
